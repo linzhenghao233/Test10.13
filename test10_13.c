1478a20ae3fc75f32b8267a0da7519566ccbb270
林政浩 <1392872316@qq.com>
2023-04-27 21:57:06 +0800

    0427

--- test10_13.c
+++ test10_13.c
@@ -8,6 +8,10 @@ int max(int*, int);
 
 int max_index(double*, int);
 
+double compare(double*, int);
+
+void oppo(double*, int);
+
 int main(void) {
 	//double source[5] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
 	//double target1[5];
@@ -22,7 +26,20 @@ int main(void) {
 	//printf("max = %d", max(arr, 5));
 
 	double arr[6] = { 1.1, 2.2, 3.3, 4.4, 5.5, 3.3 };
-	printf("最大值的下标：%d", max_index(arr, 6));
+	printf("最大值的下标：%d\n", max_index(arr, 6));
+	printf("最大值和最小值的差值：%g\n", compare(arr, 6));
+	oppo(arr, 6);
+
+	double arr_two[2][3];
+	double arr_2[2][3] = { 1.1, 2.2, 3.3, 4.4, 5.5, 3.3 };
+	for (int i = 0; i < 2; i++)
+		copy_arr(arr_two[i], arr_2[i], 3);
+	printf("%g\n", arr_two[0][1]);
+
+	double arr8[7] = { 1.1, 2.2, 3.3, 4.4, 5.5, 3.3, 2.2 };
+	double arr8_3[3] = { 0 };
+	copy_arr(arr8_3, arr8 + 2, 3);
+	printf("%g %g %g\n", arr8_3[0], arr8_3[1], arr8_3[2]);
 
 	return 0;
 }
@@ -69,4 +86,31 @@ int max_index(double* arr, int num) {
 	}
 
 	return index;
+}
+
+//5.
+double compare(double* arr, int num) {
+	double max, min, answer;
+	max = 0, min = 100;
+
+	for (int i = 0; i < num; i++) {
+		if (arr[i] > max)
+			max = arr[i];
+		if (arr[i] < min)
+			min = arr[i];
+	}
+	
+	return max - min;
+}
+
+//6.
+void oppo(double* arr, int num) {
+	int i, j;
+	double oppo_arr[6];
+
+	for (i = 0, j = num - 1; i < num; i++, j--) {
+		oppo_arr[i] = arr[j];
+		printf("%5g", oppo_arr[i]);
+	}
+	printf("\n");
 }
